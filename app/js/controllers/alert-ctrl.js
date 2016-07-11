6f778cb820740ec0999560f78d18e2136472d851
Steve Harle <stephen.harle@am.jll.com>
2016-07-11 12:49:48 -0500

    Tableau Web App

---
+++ app/js/controllers/alert-ctrl.js
@@ -0,0 +1,24 @@
+/**
+ * Alerts Controller
+ */
+
+angular
+    .module('RDash')
+    .controller('AlertsCtrl', ['$scope', AlertsCtrl]);
+
+function AlertsCtrl($scope) {
+    $scope.alerts = [{
+        type: 'success',
+        msg: 'JLLs Angular Tableau Dashboard'
+    }];
+
+    $scope.addAlert = function() {
+        $scope.alerts.push({
+            msg: 'Another alert!'
+        });
+    };
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+}
